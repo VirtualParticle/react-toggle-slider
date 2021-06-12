1dd02dee99efeb834703dd647c6bd84d5bb3f21e
Mark Vadeika <markvadeika@gmail.com>
2021-06-12 19:16:01 -0400

    removed import

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useEffect, useState } from 'react';
+import React, { CSSProperties, useState } from 'react';
 
 interface ToggleSliderHandleProps extends CSSProperties {
     size: number | string,
