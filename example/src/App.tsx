ed28aa597070b0f1a769c49786da43ef93c04f03
Mark Vadeika <markvadeika@gmail.com>
2021-06-12 21:49:19 -0400

    removed app class

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import ToggleSlider from "react-toggle-slider";
 
 function App() {
     return (
-        <div className={"App"}>
+        <div>
             <ToggleSlider/>
         </div>
     );
